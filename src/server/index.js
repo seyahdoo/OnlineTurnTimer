8f6cfc15f1fe984a4eb85f267b4748014ad66b81
Oktay Sen <oktay@oktay.io>
2020-12-27 16:51:31 +0000

    Made sockets work somehow

--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,27 @@
 const express = require('express')
 const app = express()
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+const cors = require('cors')
+
+const db = {}
+
+const socketsToRooms = {}
+
 const port = process.env.PORT || 5000
+
+app.use(cors())
 
 app.use(express.static('build'))
 
 app.get('/api/room/:roomId', (req, res) => {
     // res.send("asdasd!11s!")
-    res.send('Hello world!' + req.params.roomId)
+    res.send('11Hello world!s' + req.params.roomId)
 })
 
 app.get('/api/set-timer', (req, res) => {
@@ -17,11 +30,56 @@ app.get('/api/set-timer', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('a user connected');
-});
+    // socket.on('set', (msg) => {
+    //     console.log('set msg: ' + msg)
+    //     socket.emit('custom', "6969")
+    // })
 
-// app.listen(port, () => {
-//     console.log("App running on port", port)
-// })
+    socket.on('create_room', () => {
+        socketsToRooms[socket.id] = '1234'
+        db['1234'] = {}
+        socket.join('1234')
+        socket.to('1234').emit('room_created', {
+            room: '1234'
+        })
+    })
+
+    socket.on('join_room', (req) => {
+        const room = req.room
+        if (!room) {
+            console.log('join: No room code specified, skipping')
+            return
+        }
+        socketsToRooms[socket.id] = '1234'
+        socket.join('1234')
+        socket.to('1234').emit('room_joined', {
+            room: '1234',
+            id: socket.id
+        })
+    })
+
+    socket.on('set_timer', (room, timer) => {
+        if (!room) {
+            console.log('set: No room code specified, skipping');
+            return
+        }
+        if (!db[room]) {
+            console.log('Room', room, 'doesnt exist');
+            return
+        }
+        console.log('timer update received:', room, timer);
+        db[room].timer = timer;
+        socket.to(room).emit('timer_changed', room, timer);
+    })
+
+    socket.on('get_timer', (req) => {
+        socket.to(socket.id).emit('timer_changed', db.timer)
+    })
+
+    io.on('custom', (msg) => {
+        console.log('custom: ' + msg)
+    })
+});
 
 http.listen(port, () => {
     console.log('listening on *:', port);
--- src/web/index.js
+++ src/web/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'normalize.css/normalize.css'
+import '@blueprintjs/core/lib/css/blueprint.css'
+import '@blueprintjs/icons/lib/css/blueprint-icons.css'
+import io from "socket.io-client";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +19,16 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+let socket = io('http://localhost:5000');
+socket.on('connect', () => {
+  socket.emit('create_room')
+  socket.emit('set_timer', '1234', '232323');
+  socket.emit('custom', "232346");
+  // socket.emit('set_timer', "2323");
+});
+
+socket.on('timer_changed', (msg) => {
+  console.log('set timer to: ' + msg);
+});
+
